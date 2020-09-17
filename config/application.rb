8ce417f394b7820e3083fd62f6389d8caca3db5d
Romodanov Denis <romodanov.denis@gmail.com>
2020-09-17 12:05:53 +0500

    task 1 update 1

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,8 @@ module TestGuru
     config.time_zone = 'Ekaterinburg'
     config.default_locale = :ru
 
+    VERSION = '0.1.0'.freeze
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
