db010809b9e5653984d27859a2afecaef2aa6a38
Romodanov Denis <romodanov.denis@gmail.com>
2020-09-26 21:16:09 +0500

    commit suggestion

--- app/models/test.rb
+++ app/models/test.rb
@@ -6,7 +6,7 @@ class Test < ApplicationRecord
 
   def self.list_by_category(category_title)
     joins("INNER JOIN categories ON tests.category_id = categories.id").
-      where("categories.title = ? ", category_title ).
+      where(categories: { title: category_title }).
       order("tests.title DESC").
       pluck("tests.title")
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
 
   def linking_tests(level)
     Test.joins("INNER JOIN results On tests.id = results.test_id").
-        where("results.user_id = ?", self.id).and(Test.where(level: level))
+        where(results: { user_id: id }, level: level)
   end
 end
