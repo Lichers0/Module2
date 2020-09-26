816ea04208c96cef72345fd73cfe3e712901ae65
Romodanov Denis <romodanov.denis@gmail.com>
2020-09-26 21:07:14 +0500

    Task 5

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,8 @@
 class Test < ApplicationRecord
-  # belongs_to :category
+  belongs_to :category
+  has_many :questions
+  has_many :results
+  has_many :users, through: :results
 
   def self.list_by_category(category_title)
     joins("INNER JOIN categories ON tests.category_id = categories.id").
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :results
+  has_many :tests, through: :results
+
   def linking_tests(level)
     Test.joins("INNER JOIN results On tests.id = results.test_id").
         where("results.user_id = ?", self.id).and(Test.where(level: level))
