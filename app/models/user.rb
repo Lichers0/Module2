2eb0762d33a9e4ce895b2306555ac96541007dc8
Romodanov Denis <romodanov.denis@gmail.com>
2020-09-26 21:37:09 +0500

    add additions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :results
   has_many :tests, through: :results
+  has_many :tests_authors, class_name: "Test", foreign_key: :author_id
 
   def linking_tests(level)
     Test.joins("INNER JOIN results On tests.id = results.test_id").
