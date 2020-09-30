3fdb0a7c62eaaf1bfe7779ea599378674af71c43
Romodanov Denis <romodanov.denis@gmail.com>
2020-09-30 17:43:31 +0500

    Task6 release

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,17 @@
 class Answer < ApplicationRecord
+  MAX_COUNT_ANSWER = 4
   belongs_to :question
+
+  validates :body, presence: true
+  validates :correct_count_answer_by_question, if: :question, on: :create
+
+  scope :right_only, -> { where(correct: true) }
+
+  private
+
+  def correct_count_answer_by_question
+    if question.answers.count >= MAX_COUNT_ANSWER
+      error.messages[:answer] << "Question must have 4 answers MAX"
+    end
+  end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,7 @@
 class Category < ApplicationRecord
   has_many :tests, dependent: :destroy
+
+  validates :title, presence: true
+
+  scope :order_name_asc, -> { order(:name) }
 end
