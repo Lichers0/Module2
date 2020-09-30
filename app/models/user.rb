e80566a404499425d4bb226b3cf92f29ff04ff8f
Lichers0 <infodvd@rambler.ru>
2020-09-30 11:03:45 +0500

    Update app/models/user.rb
    Co-authored-by: max-underthesun <max.underthesun@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   has_many :tests_authors, class_name: "Test", foreign_key: :author_id, dependent: :destroy
 
   def linking_tests(level)
-    Test.joins(:results).where(level: level)
+    tests.where(level: level)
   end
 end
