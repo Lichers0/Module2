40ad418ea33a88b88c4603c0880518e6bcd58ec1
Lichers0 <infodvd@rambler.ru>
2020-09-30 11:03:29 +0500

    Update db/migrate/20200926163121_add_author_id_to_test.rb
    Co-authored-by: max-underthesun <max.underthesun@gmail.com>

--- db/migrate/20200926163121_add_author_id_to_test.rb
+++ db/migrate/20200926163121_add_author_id_to_test.rb
@@ -2,6 +2,6 @@ class AddAuthorIdToTest < ActiveRecord::Migration[6.0]
   def change
     # add_column :tests, :author_id, :integer, foreign_key: true
     # add_foreign_key :tests, :users, column: :author_id
-    add_reference :tests, :author, type: :integer, foreign_key: {to_table: :users}
+    add_reference :tests, :author, foreign_key: { to_table: :users }
   end
 end
