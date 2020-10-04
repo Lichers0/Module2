59d3a0c4ae68e58cded91993cc01d048e1da4fab
Romodanov Denis <romodanov.denis@gmail.com>
2020-10-04 20:19:14 +0500

    task 8

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,2 +1,47 @@
 class TestsController < ApplicationController
+  before_action :find_test, only: [:show, :update, :edit, :destroy]
+
+  def index
+    @tests = Test.all
+  end
+
+  def show; end
+
+  def new
+    @test = Test.new
+  end
+
+  def create
+    @test = Test.new(test_params)
+    if @test.save
+      redirect_to @test
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @test.update(test_params)
+      redirect_to @test
+    else
+      render :edit
+    end
+  end
+
+  def edit; end
+
+  def destroy
+    @test.destroy
+    redirect_to test_path
+  end
+
+  private
+
+  def find_test
+    @test = Test.find(params[:id])
+  end
+
+  def test_params
+    params.require(:test).permit(:title, :level)
+  end
 end
