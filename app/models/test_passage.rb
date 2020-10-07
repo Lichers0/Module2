0f4aca042dfdaa65395884920c92617548a1d0b0
Romodanov Denis <romodanov.denis@gmail.com>
2020-10-07 18:01:30 +0500

    task9 final

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -1,9 +1,11 @@
 class TestPassage < ApplicationRecord
+  SUCCESS_TEST_LEVEL = 85.freeze
+
   belongs_to :user
   belongs_to :test
   belongs_to :current_question, class_name: "Question", optional: true
 
-  before_validation :before_validation_set_first_question , on: :create
+  before_validation :before_validation_set_first_question
 
   def completed?
     current_question.nil?
@@ -13,21 +15,36 @@ class TestPassage < ApplicationRecord
     if correct_answer?(answer_ids)
       self.correct_questions += 1
     end
-
-    self.current_question = next_question
     save!
+  end
+
+  def current_test_level
+    (correct_questions.to_f / test.questions.count * 100).to_i
+  end
+
+  def passed?
+    current_test_level >= SUCCESS_TEST_LEVEL
+  end
+
+  def index_current_question
+    test.questions.index(current_question) + 1
+  end
+
+  def total_test_questions
+    test.questions.count
   end
 
   private
 
   def before_validation_set_first_question
-    self.current_question = test.questions.first if test.present?
+    self.current_question = new_record? ? test.questions.first : next_question
   end
 
   def correct_answer?(answer_ids)
-    correct_answers_count = correct_answers.count
-    correct_answers_count == correct_answers.where(id: answer_ids).count &&
-        correct_answers_count == answer_ids.count
+    # correct_answers_count = correct_answers.count
+    # correct_answers_count == correct_answers.where(id: answer_ids).count &&
+    #     correct_answers_count == answer_ids.count
+    correct_answers.ids.sort == answer_ids.map(&:to_i).sort
   end
 
   def correct_answers
